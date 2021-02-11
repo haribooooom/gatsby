9a3c50c63b0e3a23a2fb9de40476a06802bbf41f
noma <>
2021-02-11 10:11:58 +0900

    config add icon

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,4 +7,15 @@
 module.exports = {
   /* Your site config here */
   plugins: [],
-}
+
+  resolve: `gatsby-plugin-manifest`,
+  options: {
+    name: `noma log`,
+    short_name: `GatsbyJS`,
+    start_url: `/`,
+    background_color: `#ffffff`,
+    theme_color: `#44cb7d`,
+    display: `minimal-ui`,
+    icon: `/static/icon.ico`, // ← ここ！！
+  },
+},
